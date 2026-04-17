f07dc7050760ea5790746695cf1e0e93d15004b2
fawwazgoreng <hatorimikael@gmail.com>
2026-04-17 20:39:43 +0700

    feat: add not found page and roue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "@/pages/Index";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="*" element={<NotFound />}></Route>
             </Routes>
         </Router>
     );
